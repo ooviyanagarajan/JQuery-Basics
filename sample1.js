0cf9f854c3d591d16d22e4764f88f94de914c5ee
ooviyanagarajan <46079755+ooviyanagarajan@users.noreply.github.com>
2019-09-19 23:05:01 +0530

    Add files via upload

---
+++ sample1.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+
+    console.log("hello");
+    $("h1").text(); //returns "jQuery Basics"
+
+    //text() allows you to modify the text node
+    $("h1").text("Hello JQuery")
+
+});
